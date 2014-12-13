2777bf5dbd59325d6b5e27326206a6d6f3889d7b
kymogram <jberrewa@ulb.ac.be>
2014-12-13 20:03:11 +0100

    Projet d'année presque la fin, travail sur update, sous le fichier rumeur_pers et la fonction update de rumorfunctionpart2 n'est pas juste pour l'instant

---
+++ rumeur_pers.py
@@ -0,0 +1,71 @@
+import rumorFunctionsPart2
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", type=str, default = "stable", help = "Règle de " + \
+                    "mise à jour lorsqu'une personne apprend une nouvelle " + \
+                    "version de la rumeur (choix : stable, rewrite, mixture)")
+args = parser.parse_args()
+
+nom_pers, reseau = rumorFunctionsPart2.loadNetwork("name.txt")
+pers_info = [False for pers in range(len(nom_pers))]
+
+def si_connait_pas(pers_info, args, val_rumeur, alea):
+    if args.m == "none":
+        val_rumeur[alea] = args.r
+    elif args.m == "incremental":
+        val_rumeur[alea] = incremental(args)
+    elif args.m == "bitflip":
+        val_rumeur[alea] = convert_bin(bit_flip(args))
+    pers_info[alea] = True
+    return val_rumeur, pers_info
+
+def rumeur_pers(nom_pers, reseau):
+    val_rumeur = [[] for i in range(len(nom_pers))]
+    for pers in range(len(nom_pers)):
+        if pers_info[pers]:
+            alea = randint(0,len(nom_pers)-1)
+            #Tant que c'est lui-même ou quelqu'un avec qui il n'est pas ami
+            #La valeur aléatoire change pour qu'il puisse la transmettre à
+            #Quelqu'un d'adéquat
+            while pers == alea or not reseau[alea][pers]:
+                alea = randint(0, len(pers_info)-1)
+            proba_rumeur = 10*args.p
+            val_alea_rumeur = randint(1,10//proba_rumeur)
+            #1 étant un chiffre aléatoire qui a une chance d'arrivé par rapport
+            #à args.p et par rapport aux caculs ci-dessus
+            if val_alea_rumeur == 1:
+                #Si l'option "Don't tell" est choisi, seules les personnes ne
+                #connaissant pas la rumeur, peuventl'apprend.
+                #Les autres gardent celle qu'ils avaient
+                if args.d:
+                    if not pers_info[alea]:
+                        val_rumeur, pers_info = \
+                            si_connait_pas(pers_info, args, val_rumeur, alea)
+                else:
+                    if pers_info[alea]:
+                        if args.u == "rewrite":
+                            val_rumeur[alea] = val_rumeur[pers]
+                        elif args.u == "mixture":
+                            dec_rumor = args.r
+                            bin_rumor = convert_dec(dec_rumor)
+                            for elem in bin_rumor:
+                                #pour la probabilité de 1/9 : si on fait un
+                                #randint de 0 à 9, chaque chiffre a une
+                                #possibilité de 1/10 d'apparaitre
+                                proba = randint(0,9)
+                                if val_rumeur[pers][elem] != \
+                                                val_rumeur[alea][elem]:
+                                    if proba == 0
+                                        val_rumeur[alea][elem] = \
+                                                val_rumeur[pers][elem]
+                    else:
+                        val_rumeur, pers_info = \
+                            si_connait_pas(pers_info, args, val_rumeur, alea)
+            else:
+                val_rumeur[pers] = args.r
+        else:
+            val_rumeur[pers] = "Ne connait pas la rumeur"
+    return val_rumeur
+    
+
+print(rumeur_pers(nom_pers))
---
+++ rumorFunctionsPart2.py
@@ -0,0 +1,181 @@
+from random import randint
+    
+def loadNetwork(nom_fichier):
+    nom_pers = []
+    fichier = open(nom_fichier)
+    ligne = [line.strip() for line in fichier.readlines()]
+    fichier.close()
+    nbr_pers_res = len(ligne)
+    reseau = [[0 for j in range(nbr_pers_res)]for i in range(nbr_pers_res)]
+    #J'initialise le nom des personnes dans la liste nom_pers
+    for i in range(nbr_pers_res):
+        reseau[i][i] = True
+        pers = ligne[i].split(':')
+        nom = str(pers[0]).strip()
+        nom_pers.append(nom)
+    
+    for i in range(nbr_pers_res):
+        pers = ligne[i].split(':')
+        ami = pers[1].split(',')
+        #Je prends la liste de la personne a en ami et je l'isole et la nettoie
+        #Dans le for
+        for j in range(len(ami)):
+            ami[j] = "".join(ami[j]).strip()
+        for j in range(len(ami)):
+            if ami[j] in nom_pers:
+                for k in range(nbr_pers_res):
+                    if i != k:
+                        if nom_pers[k] in ami:
+                            reseau[i][k] = True
+                        else:
+                            reseau[i][k] = False
+    return nom_pers, reseau
+    
+def incremental(args):
+    dec_rumor = args.r
+    incr_decr = randint(0,1)
+    if dec_rumor == 255:
+        dec_rumor -= 1
+    elif dec_rumor == 0:
+        dec_rumor += 1
+    else:
+        if incr_decr == 0:
+            dec_rumor -= 1
+        if incr_decr == 1:
+            dec_rumor += 1
+    return dec_rumor
+
+def convert_dec(dec_rumor):
+    str_bin = ""
+    bin_rumor = list(bin(dec_rumor)[2:])
+    while len(bin_rumor) < 8:
+        add_zero = ['0']
+        bin_rumor = add_zero + bin_rumor[:]
+    return bin_rumor
+    
+def convert_bin(bin_rumor):
+    dec_rumor = int(bin_rumor, 2)
+    return dec_rumor
+  
+def str_bin(bin_rumor):
+    str_bin = ""
+    for elem in bin_rumor:
+        str_bin += str(elem)
+    return str_bin
+  
+def bit_flip(args):
+    dec_rumor = args.r
+    bin_rumor = convert_dec(dec_rumor)
+    alea_bit_flip = randint(0,7)
+    
+    if bin_rumor[alea_bit_flip] == '0':
+        bin_rumor[alea_bit_flip] = 1
+    else:
+        bin_rumor[alea_bit_flip] = 0
+    bin_rumor = str_bin(bin_rumor)
+    
+    args.r = convert_bin(bin_rumor)
+    return bin_rumor
+    
+def rumeur_pers(nom_pers, dec_rumor):
+    val_rumeur = [[] for i in range(len(nom_pers))]
+    
+def variable_arg():
+    #Variable pour '-s'
+    alea_indice = randint(0,len(nom_pers)-1)
+    #Variable pour '-r'
+    alea_init = randint(0,255)
+    #Variable pour '-t'
+    nbr_simu = 0
+    while count != len(pers_info):
+        count = 0
+        for elem in pers_info:
+            if elem:
+                count += 1
+        nbr_simu += 1
+    return alea_indice, alea_init, nbr_simu
+    
+def verification_arg(args):
+    if args.s in nom_pers:
+        for pers in range(len(nom_pers)):
+            if nom_pers[pers] == args.s:
+                pers_info[pers] = True
+    else:
+        raise ValueError("Cette personne n'existe pas dans votre réseau")
+            
+    if args.r < 0:
+        print("\nSoyez raisonnable...\n")
+        raise ValueError("Aucun nombre négatif n'est toléré")
+    elif args.r > 255:
+        print("\nVeuillez mettre une valeur ne dépassant pas 255\n")
+        raise ValueError("Votre valeur est trop grande")
+    
+    if args.p < 0:
+        print("\nSoyez raisonnable...\n")
+        raise ValueError("Aucun nombre négatif n'est toleré")
+    elif args.p > 1:
+        print("\nVeuillez mettre une valeur ne dépassant pas 1\n")
+        raise ValueError("Votre valeur est trop grande")
+        
+    if args.m == "incremental":
+        args.m = incremental(args)
+    elif args.m == "bitflip":
+        args.m = bit_flip(args)
+    elif args.m == "none":
+        args.m = "none"
+    else:
+        print("\nSi vous ne savez pas quoi faire, demandez l'aide avec '-h'\n")
+        raise ValueError("le paramètre choisi est incorrect")
+    return args.r, args.r, args.p, args.m
+
+def fichier_printer(nom_fichier):
+    fichier = open(nom_fichier)
+    ligne = fichier.readlines()
+    document = ""
+    for elem in ligne:
+        document += str(elem)
+    return document
+    
+def printState(nom_pers, pers_info):
+    #J'initialise à 20 le nombre d'espace qu'il faut mettre après le nom
+    espace_ecart = 20
+    print("le réseau est le suivant :\n" + fichier_printer(nom_fichier) + "\n")
+    ini_rumor = input("Personne initiant la rumeur: ")
+    #Petite vérification
+    while ini_rumor != ini_rumor.isalpha():
+        ini_rumor = input("Personne initiant la rumeur: ")
+    dec_rumor = randint(0,255)
+    print("Valeur de la rumeur initiale: " + str(dec_rumor) + \
+          "\nNom\t\tBIN\tDEC")
+    for pers in range(len(nom_pers)):
+        espace_ecart -= len(nom_pers[pers])
+        #Verifie si la personne connait la rumeur
+        if pers_info[pers]:
+            print(str(nom_pers[pers]) + espace_ecart + bin_rumor(dec_rumor))
+        else:
+            print(str(nom_pers[pers]) + " ne connaît pas la rumeur")
+
+def update(reseau, pers_info):
+    #Variable qui va compter combien de gens vont apprendre la rumeur à chaque
+    #Appel à la fonction
+    pers_connait = 0
+    for pers in range(len(pers_info)):
+        #Si la personne connait la rumeur, il peut la transmettre
+        if pers_info[pers]:
+            alea = randint(0,len(pers_info)-1)
+            if not args.d:
+                while pers == alea or not reseau[alea][pers]:
+                    alea = randint(0, len(pers_info)-1)
+                if not pers_info[alea]:
+                    pers_connait += 1
+                pers_info[alea] = True
+                
+            #Si l'option don't tell again a été choisi, seul les personne ne
+            #Connaissant pas la rumeur peuvent recevoir la rumeur
+            else:
+                while pers == alea or not reseau[alea][pers] \
+                                        or not pers_info[alea]:
+                    alea = randint(0, len(pers_info)-1)
+                pers_connait += 1
+                pers_info[alea] = True
+    return pers_connait
